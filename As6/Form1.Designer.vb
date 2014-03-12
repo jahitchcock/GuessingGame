5b68d392063482bf0fd831dd02a62f563047d1eb
jahitchcock <joshua.hitchcock@live.com>
2014-03-12 19:32:09 -0400

    Add to repository
    A simple guessing game created for CIS 130 at HFCC

---
+++ As6/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGuess = New System.Windows.Forms.Button
+        Me.btnGiveUp = New System.Windows.Forms.Button
+        Me.btnExit = New System.Windows.Forms.Button
+        Me.lblGuess = New System.Windows.Forms.Label
+        Me.txtGuess = New System.Windows.Forms.TextBox
+        Me.SuspendLayout()
+        '
+        'btnGuess
+        '
+        Me.btnGuess.Location = New System.Drawing.Point(167, 12)
+        Me.btnGuess.Name = "btnGuess"
+        Me.btnGuess.Size = New System.Drawing.Size(75, 23)
+        Me.btnGuess.TabIndex = 2
+        Me.btnGuess.Text = "&Guess"
+        Me.btnGuess.UseVisualStyleBackColor = True
+        '
+        'btnGiveUp
+        '
+        Me.btnGiveUp.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnGiveUp.Location = New System.Drawing.Point(167, 41)
+        Me.btnGiveUp.Name = "btnGiveUp"
+        Me.btnGiveUp.Size = New System.Drawing.Size(75, 23)
+        Me.btnGiveUp.TabIndex = 3
+        Me.btnGiveUp.Text = "Give &up"
+        Me.btnGiveUp.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(167, 70)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblGuess
+        '
+        Me.lblGuess.AutoSize = True
+        Me.lblGuess.Location = New System.Drawing.Point(22, 22)
+        Me.lblGuess.Name = "lblGuess"
+        Me.lblGuess.Size = New System.Drawing.Size(89, 13)
+        Me.lblGuess.TabIndex = 0
+        Me.lblGuess.Text = "&Enter your guess:"
+        '
+        'txtGuess
+        '
+        Me.txtGuess.Location = New System.Drawing.Point(16, 61)
+        Me.txtGuess.Name = "txtGuess"
+        Me.txtGuess.Size = New System.Drawing.Size(100, 20)
+        Me.txtGuess.TabIndex = 1
+        '
+        'frmMain
+        '
+        Me.AcceptButton = Me.btnGuess
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnGiveUp
+        Me.ClientSize = New System.Drawing.Size(261, 103)
+        Me.Controls.Add(Me.txtGuess)
+        Me.Controls.Add(Me.lblGuess)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnGiveUp)
+        Me.Controls.Add(Me.btnGuess)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Guess"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnGuess As System.Windows.Forms.Button
+    Friend WithEvents btnGiveUp As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblGuess As System.Windows.Forms.Label
+    Friend WithEvents txtGuess As System.Windows.Forms.TextBox
+
+End Class
---
+++ As6/Form1.vb
@@ -0,0 +1,58 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class frmMain
+    Dim rndm As New Random
+    Dim dblNumber As Double
+    Dim dblCount As Double = 0
+
+    Private Sub btnGuess_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGuess.Click
+        Dim dblGuess As Double = (Val(txtGuess.Text))
+        Call guess(dblGuess, dblNumber)
+        dblCount = Heckle(dblCount)
+        txtGuess.Text = ""
+    End Sub
+
+    Private Sub frmMain_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        dblNumber = rndm.Next(1, 100)
+
+    End Sub
+
+    Private Sub btnGiveUp_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGiveUp.Click
+        MessageBox.Show("The number was " & dblNumber & " you gave up after " & dblCount & " guesses!", "I Win, You Lose!", MessageBoxButtons.OK)
+        dblNumber = rndm.Next(1, 100)
+        dblCount = 0
+        txtGuess.Text = " "
+    End Sub
+
+    Private Sub btnExit_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnExit.Click
+        MessageBox.Show("Giving up already", "Quit?", MessageBoxButtons.OK)
+        Me.Close()
+    End Sub
+    Public Function Heckle(ByVal dblCount As Double) As Double
+        Select Case dblCount
+            Case Is = 4
+                MessageBox.Show("You guessed five times already!", "Ha Ha Ha Ha Ha", MessageBoxButtons.OK)
+            Case Is = 9
+                MessageBox.Show("10 Guesses!! How pathetic!!", "Loser!", MessageBoxButtons.OK)
+            Case Is = 14
+                MessageBox.Show("That is just sad, 15 guesses!", "Face Palm", MessageBoxButtons.OK)
+            Case Is = 19
+                MessageBox.Show("You are either really determined or just stupid!", "20 Guesses", MessageBoxButtons.OK)
+            Case Is = 29
+                MessageBox.Show("Yeah, stupid it is!!!", "30 Guesses!", MessageBoxButtons.OK)
+        End Select
+        Return (dblCount + 1)
+    End Function
+    Public Sub guess(ByVal dblGuess As Double, ByVal dblNumber As Double)
+        Select Case dblGuess
+            Case Is = dblNumber
+                MessageBox.Show("Hey you're not as dumb as you look!", "Winner! " & dblCount & " guesses!", MessageBoxButtons.OK)
+
+            Case Is > dblNumber
+                MessageBox.Show("Hey bozo, your guess is too high!", "Too High!", MessageBoxButtons.OK)
+            Case Is < dblNumber
+                MessageBox.Show("What is wrong with you? Guess higher next time!", "Too Low", MessageBoxButtons.OK)
+        End Select
+    End Sub
+End Class
